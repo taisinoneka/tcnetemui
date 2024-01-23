7b298e8527ecbd533020ac6d9bcba9aa9e91b5c7
Aksels Skvorcovs <aksels.skvorcovs@indexo.lv>
2024-01-23 12:49:29 +0200

    Removed unused code and added comment

--- luasrc/controller/tcnetemui.lua
+++ luasrc/controller/tcnetemui.lua
@@ -1,16 +1,7 @@
--- /usr/lib/lua/luci/controller/example.lua
 module("luci.controller.tcnetemui", package.seeall)
 local uci = require("luci.model.uci").cursor()
 
+-- Sets entry point in LuCi portal
 function index()
     entry({"admin", "tcnetemui"}, cbi("tcnetemui",{hidesavebtn=true, hideresetbtn=true, hideapplybtn=true}), "tc-netem-ui").index = true
-end
-
-function reset()
-    luci.http.write("<p>ALL LIMITATIONS REMOVED</p>")
-    luci.sys.exec(string.format("tc qdisc del dev %s root", uci:get("network", "lan", "device")))
-    luci.sys.exec(string.format("tc qdisc del dev %s root", uci:get("network", "wan", "device")))
-    luci.sys.exec("tc qdisc")
-    local log = luci.sys.exec("tc qdisc show")
-    luci.http.write(log)
 end
